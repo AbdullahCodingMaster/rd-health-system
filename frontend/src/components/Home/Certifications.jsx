fd0b2f8d147c1e5ca6253df0b55e9e58e6752457
hassanmunir083@gmail.com <hassanmunir083@gmail.com>
2024-09-16 13:58:27 +0500

    adding home components

---
+++ frontend/src/components/Home/Certifications.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import c1 from "../../assets/c1.png"
+import C2 from "../../assets/C2.png"
+import c3 from "../../assets/c3.png"
+import c4 from "../../assets/c4.png"
+import c5 from "../../assets/c5.png"
+const Certifications = () => {
+  return (
+    <div>
+      certifications
+        
+
+    </div>
+  )
+}
+
+export default Certifications
